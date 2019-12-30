164f2e665a39d529ce27e44b1ef7225a0011c0fb
aiviskless <aiviskle@gmail.com>
2019-12-30 19:17:39 +0200

    Add libs

---
+++ libs.h
@@ -0,0 +1,14 @@
+#ifndef LABIRINTH_CLIENT_LIBS_H
+#define LABIRINTH_CLIENT_LIBS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <ncurses.h>
+
+#endif
