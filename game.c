bf513a549e4bb84308f96fc5e3e303d1492904e6
aiviskless <aiviskle@gmail.com>
2020-01-13 23:34:26 +0200

    add more comments

--- game.c
+++ game.c
@@ -3,8 +3,10 @@
 #include "consts.h"
 #include "maps.c"
 
+// center of 'tile'
 struct pair TILE_CENTER = { 3*PIXEL_SIZE, 4*PIXEL_SIZE };
 
+// map coordinates where new food will get spawned
 struct pair FOOD_LOCATIONS[] = {
 	{1, 3},
 	{2, 8},
@@ -20,6 +22,7 @@ struct pair FOOD_LOCATIONS[] = {
 	{2, 14},
 };
 
+// ncurses window
 struct window {
 	int top, left;
 	struct pair camera_target_tile;
@@ -27,7 +30,7 @@ struct window {
 
 const struct level new_level;
 
-// pairs for colors
+// enum for colors
 enum {
 	BG_PAIR = 1,
 	PLAYER_PAIR,
@@ -65,6 +68,7 @@ void generate_food() {
 	arena_map[x*ARENA_WIDTH_IN_TILES + y] = '.';
 }
 
+// return map's tile
 char get_arena(int row, int col) {
 	if (col < 0) {
 		col += ARENA_WIDTH_IN_TILES;
@@ -74,6 +78,7 @@ char get_arena(int row, int col) {
 	return arena_map[row*ARENA_WIDTH_IN_TILES + col];
 }
 
+// return window position where to draw something 
 struct pair draw_pos(int row, int col, struct window* window) {
 	struct pair ret = {
 		3 * (col - window->camera_target_tile.x) + window->left,
@@ -122,19 +127,15 @@ struct pair pos_to_tile(struct pair* p) {
 	return div_scale(*p, TILE_SIZE);
 }
 
-bool test_player_dir_blocked(enum dir dir, struct pair* next_pos) {
+bool is_player_dir_blocked(enum dir dir, struct pair* next_pos) {
 	int mv_amt = TILE_SIZE / 2;
 	if (dir == RIGHT || dir == UP) {
 		mv_amt += PIXEL_SIZE;
 	}
 	move_in_dir(dir, next_pos, mv_amt);
 	struct pair next_tile = pos_to_tile(next_pos);
-	// move chars to const
-	return !!strchr(ARENA_WALLS, get_arena(next_tile.y, next_tile.x));
-}
 
-int seconds_to_frames(int seconds) {
-	return SECONDS(seconds) / MS(16);
+	return !!strchr(ARENA_WALLS, get_arena(next_tile.y, next_tile.x));
 }
 
 struct game_data {
@@ -176,7 +177,6 @@ struct game_data create_new_game() {
 }
 
 int start_game() {
-	// printf("%d\n", FOOD_LOCATIONS[0].x);
 	// Init curses
 	initscr();
 	cbreak();
@@ -187,7 +187,7 @@ int start_game() {
 
 	int row, col;
 
-	// kontrolē PAUSED_BEFORE_PLAYING ilgumu
+	// controls PAUSED_BEFORE_PLAYING duration
 	int transition_timer = 0;
 
 	bool running = TRUE;
@@ -197,15 +197,15 @@ int start_game() {
 	int next_input = -1;
 	struct window window = { 0 };
 
-	// uzstāda krāsas jeb krāsu pārus
+	// setup colors
 	start_color();
-
 	// init_pair(index, foreground, background);
 	init_pair(BG_PAIR, COLOR_WHITE, COLOR_BLACK_DARK);
 	init_pair(PLAYER_PAIR, COLOR_YELLOW_BRIGHT, COLOR_GRAY);
 	init_pair(FOOD_PAIR, COLOR_ORANGE, COLOR_GRAY);
 	init_pair(WALL_PAIR, COLOR_WHITE, COLOR_GRAY);
 
+	// set background color
 	bkgd(COLOR_PAIR(BG_PAIR));
 
 	struct game_data game = create_new_game();
@@ -215,7 +215,7 @@ int start_game() {
 
 	int frame_time = 16667;
 	while (running) {
-		// Iegūt input
+		// get input
 		int ch;
 		while ((ch = getch()) != -1) {
 			switch (ch) {
@@ -234,6 +234,7 @@ int start_game() {
 			}
 		}
 
+		// if player doesn't move
 		if (!game.level.player_turning && next_input != -1) {
 			game.level.next_dir = next_input;
 			next_input = -1;
@@ -255,12 +256,15 @@ int start_game() {
 				case PLAYING: {
 					int old_score = game.score;
 					
+					// movement
 					if (game.level.player_dir != game.level.next_dir) {
 						if (is_direction_horizontal(game.level.player_dir) == is_direction_horizontal(game.level.next_dir)) {
 							game.level.player_dir = game.level.next_dir;
 						} else {
+							// player changes direction
 							struct pair blocked_pos_s = game.level.player_pos;
-							bool blocked = test_player_dir_blocked(game.level.next_dir, &blocked_pos_s);
+							bool blocked = is_player_dir_blocked(game.level.next_dir, &blocked_pos_s);
+							// check if player is walking towards a wall
 							if (!blocked) {
 								game.level.player_turning = TRUE;
 								player_turn_tile = pos_to_tile(&game.level.player_pos);
@@ -268,12 +272,13 @@ int start_game() {
 						}
 					}
 
-					// kustība
+					// player changed direction
 					if (game.level.player_turning) {
 						move_in_dir(game.level.next_dir, &game.level.player_pos, PLAYER_SPEED);
 						struct pair tile_center = add(scale(player_turn_tile, TILE_SIZE), TILE_CENTER);
 						bool reached_centerline = FALSE;
 						if (is_direction_horizontal(game.level.player_dir)) {
+							// horizontal direction
 							int mv_amt = tile_center.x - game.level.player_pos.x;
 							if (mv_amt < 0) {
 								game.level.player_pos.x -= MIN(-mv_amt, PLAYER_SPEED);
@@ -283,6 +288,7 @@ int start_game() {
 								reached_centerline = mv_amt < PLAYER_SPEED;
 							}
 						} else {
+							// vertical direction
 							int mv_amt = tile_center.y - game.level.player_pos.y;
 							if (mv_amt < 0) {
 								game.level.player_pos.y -= MIN(-mv_amt, PLAYER_SPEED);
@@ -298,23 +304,26 @@ int start_game() {
 							game.level.player_dir = game.level.next_dir;
 						}
 					} else {
+						// check if player is blocked by a wall
 						struct pair blocked_pos_s = game.level.player_pos;
-						game.level.player_blocked = test_player_dir_blocked(game.level.player_dir, &blocked_pos_s);
+						game.level.player_blocked = is_player_dir_blocked(game.level.player_dir, &blocked_pos_s);
 						if (!game.level.player_blocked) {
 							move_in_dir(game.level.player_dir, &game.level.player_pos, PLAYER_SPEED);
 						}
 					}
 
-					// pārbauda, vai uzkāpj uz ēdiena
+					// get player's tile position and char that has been stepped on
 					struct pair player_tile = pos_to_tile(&game.level.player_pos);
 					char ch = get_arena(player_tile.y, player_tile.x);
+					
+					// check if player stepped on food
 					if (ch == '.') {
-						// noņem ēdienu
+						// remove food from map
 						arena_map[player_tile.y*ARENA_WIDTH_IN_TILES + player_tile.x] = ' ';
 						
 						game.score += 10;
 
-						// uzģenerē jaunu ēdienu nejaušā vietā
+						// generate new food in a random place
 						generate_food();
 					}
 				} break;
@@ -322,11 +331,11 @@ int start_game() {
 
 			erase();
 
-			// nobīde no malām
+			// margins
 			window.left = 3;
 			window.top = 3;
 
-			// uzzīmēt karti
+			// draw map
 			for (row = 0; row < ARENA_HEIGHT_IN_TILES; ++row) {
 				for (col = 0; col < ARENA_WIDTH_IN_TILES; ++col) {
 					char ch = get_arena(row, col), draw_ch = ch, fill_ch = ' ';
@@ -347,7 +356,7 @@ int start_game() {
 							attron(COLOR_PAIR(WALL_PAIR));
 							break;
 					}
-					// ja '-', ieliek vēl vienu '-', lai labāk izskatās
+					// if '-', add another '-' so it looks better
 					if (ch == '-') {
 						fill_ch = ch;
 					}
@@ -360,24 +369,22 @@ int start_game() {
 
 			char player_symbol = 'A';
 
-			// uzzīmē spēlētāja simbolu uz kartes
+			// draw player's symbol on map
 			struct pair player_tile = pos_to_tile(&game.level.player_pos);
 			if (player_tile.x >= 0 && player_tile.x < ARENA_WIDTH_IN_TILES) {
 				draw_tile_pair(player_tile, player_symbol, &window, ' ');
 			}
 
-			// spēles sākuma statuss
+			// game status before playing
 			if (game.mode == PAUSED_BEFORE_PLAYING) {
 				struct pair pos = draw_pos(14, 16, &window);
 				mvprintw(pos.y, pos.x, "R E A D Y !");
 			}
 
-			// spēles statuss
+			// game status while playing
 			attron(COLOR_PAIR(BG_PAIR));
 			mvprintw(window.top - 2, window.left + 2, "Score");
 			mvprintw(window.top - 2, window.left + 7, "%4d",  game.score);
-
-			int diag_row = window.top + ARENA_HEIGHT_IN_TILES + 1;
 
 			refresh();
 
